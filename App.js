cb5eb68cec118d126d90890af13bc8dad27e9988
antonidmar <47029503+antonidmar@users.noreply.github.com>
2019-10-28 00:08:10 +0100

    Add files via upload

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import axios from "axios";
 
 
 
+
 // const url ="http://localhost:3001/api/lime";
 // const response =  fetch(url, {
 //      method: "GET", // *GET, POST, PUT, DELETE, etc.
@@ -14,9 +15,14 @@ import axios from "axios";
 //       "x-api-key": apiKey
 //      // 'Content-Type': 'application/x-www-form-urlencoded',
 // }});
+//creating a const for the proxy-server and the standard way into the API.
 const PROXY_URL = "http://localhost:3001/api/lime/"
 const API_URL = "https://api-test.lime-crm.com/api-test/api/v1/"
 
+
+
+
+//Giving all the parameters that's later gonna be my "keyvalues" a value so that I can change the state later.
 export default class App extends Component {
   constructor(props) {
     super(props);
@@ -30,6 +36,7 @@ export default class App extends Component {
     };
   }
 
+  //Getting the specific data thaht i'm searching for and setting the state for my functions. 
   fetchLimeObjects(url, params) {
     axios
       .get(
@@ -57,7 +64,7 @@ export default class App extends Component {
         console.log(error);
       });
   }
-
+  //Get the array limeobject and filter so that I just get the closeddate == 2018, and then map them to the their value, returning all the values and splittning them witb 12.
   averageDealValue(data){
     const deals = 
       data.filter(deal => {
@@ -81,9 +88,11 @@ export default class App extends Component {
     data.filter(deal => {
       const closedDate = new Date(deal.closeddate);
       return closedDate.getFullYear() === 2018
-      }).map(deal => deal._id && deal.company)
+      }).map(deal => deal.company )
       return deals + ','
     }
+
+
 
   companiesWonDeals(data) {
     const deals =
@@ -101,13 +110,15 @@ export default class App extends Component {
     return deals 
   }
 
+  //this function is gonna work so we can get more than the first 10 from the limeobjects.
   onLoadMore(event){
     const [url, params] = this.state.next.split('?')
     this.fetchLimeObjects(url, params)
   }
-
+  //After all the elements of the page is rendered correctly, this method is called. After the markup is set on the page, this technique called by React itself to either fetch the data from An External API or perform some unique operations which need the JSX elements.
   componentDidMount() {
-    this.fetchLimeObjects("http://localhost:3001/api/lime/limeobject/deal/", "_limit=10&_sort=-closeddate&_embed=company") 
+    this.fetchLimeObjects("http://localhost:3001/api/lime/limeobject/deal/",
+     "_limit=10&_sort=-closeddate&_embed=company") 
     // axios
     //   .get(
     //     "http://localhost:3001/api/lime/limeobject/deal/?_limit=10&_sort=-closeddate&_embed=company"
@@ -124,26 +135,65 @@ export default class App extends Component {
     //   });
   }
 
+ 
+
   render() {
     const { limers, average, averagePerMonth, companies, customersWonDeal } = this.state;
-    return (
-      //Styling
-      <div className="App">
-        
+    return ( 
+      //Strucutirng and visualizing the data
+        <div className="App">
+          <div className="header">
+            <h1>Dream Team</h1>
+            <p>- "Making dreams become reality"</p>
+          </div>
 
-        <h1>{ average }</h1>
-        
-        
-        <button onClick = {this.onLoadMore.bind(this)}>load more</button>
-        
-         <p className="month">{ averagePerMonth }</p>
-       
-       <p className="customer">{ companies }</p>
-       
-         <ul className="customersWonDeals">{ Object.entries(customersWonDeal).map(([k, v]) => (<li>{k}: {v}</li>)) }</ul>
-       
-      </div>
-    );
+        <div class="row">
+          <div class="column"> 
+            <div class="card1">
+              <h2>Average deal value for won deals 2018:</h2>
+              <h3> {average}:- </h3></div>
+          </div>
+          <div class="column">
+            <div class="card2">
+              <h2>Average deal value per month 2018:</h2>
+              <h3>{ averagePerMonth }:-</h3></div>
+          </div>
+          <div class="column">
+            <div class="card3"><h2>Company-ID</h2>
+              <li><h3>{ companies }</h3></li></div>
+          </div>
+          <div class="column">
+            <div class="card4">
+              <h2>The value of won deals per customer 2018:</h2>
+               <ul><h3>{ Object.entries(customersWonDeal).map(([k, v]) => (<li>{k}: {v}</li>)) }</h3></ul></div>
+          </div>
+        </div>
+      
+        <footer>
+          <div className="contact-us">
+           <h4>Contact us</h4>
+            <i class="material-icons">phone</i>
+            <p>031-902311</p>
+          </div>
+          <div className="follow-us">   
+              <li>
+                <h4>Follow us</h4>
+                <i class="material-icons">camera_alt</i>
+                <i class="material-icons">public</i>
+                <i class="material-icons">subscriptions</i>
+                <p>@dreamteam</p>
+              </li>
+          </div>
+          <div className="verified">      
+              <h4>Verified dreamers</h4>
+              <i className="material-icons">cloud_done</i>
+              <p>dreamteam@info.com</p>    
+          </div>    
+        </footer>
+         </div>
+             
+      
+    )
     
   }
 }
